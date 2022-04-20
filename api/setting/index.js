bce9d62ebc5d51dba69856209c37a29c4965dd57
Alan Borsato <37001712+aborsato@users.noreply.github.com>
2022-04-20 18:57:15 +0000

    add api for settings

---
+++ api/setting/index.js
@@ -0,0 +1,15 @@
+
+// Export our function
+module.exports = async function (context, req) {
+    // setup our default content type (we always return JSON)
+    context.res = {
+        header: {
+            "Content-Type": "application/json"
+        }
+    }
+
+    // Grab the id from the URL (stored in bindingData)
+    const id = context.bindingData.id;
+
+    context.res.body = { setting: id, value: process.env[id] };
+};
